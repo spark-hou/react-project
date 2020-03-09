fc87fdad4a54832817e64b7a11d0da240ea1b7d1
侯欣榆 <xinyu.hou@dhc.com.cn>
2020-03-09 14:41:09 +0800

    修改ignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './App.scss';
 import {routes} from "./router/config"
-import {Route, Switch,withRouter} from "react-router-dom";
+import {Route, Switch, withRouter} from "react-router-dom";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        props.history.listen(()=>{  //在这里监听location对象
+        props.history.listen(() => {  //在这里监听location对象
             console.log(this.props);  //切换路由的时候输出"/one/users"和"/one/companies"
         })
         this.state = {
@@ -15,35 +15,33 @@ class App extends Component {
     }
 
 
-
     render() {
         return (
             <div className="App">
 
-                    <Switch>
-                        {
-                            routes.map((item, i) => {
-                                    if (item.exact) {
-                                        return <Route path={item.path}
-                                                      exact
-                                                      key={i}
-                                                      render={props => (
-                                                          <item.component {...props} routes={item.routes}/>
-                                                      )}
-                                        />
-                                    } else {
-                                        return <Route path={item.path}
-                                                      key={i}
-                                                      render={props => (
-                                                          <item.component {...props} routes={item.routes}/>
-                                                      )}
-                                        />
-                                    }
+                <Switch>
+                    {
+                        routes.map((item, i) => {
+                                if (item.exact) {
+                                    return <Route path={item.path}
+                                                  exact
+                                                  key={i}
+                                                  render={props => (
+                                                      <item.component {...props} routes={item.routes}/>
+                                                  )}
+                                    />
+                                } else {
+                                    return <Route path={item.path}
+                                                  key={i}
+                                                  render={props => (
+                                                      <item.component {...props} routes={item.routes}/>
+                                                  )}
+                                    />
                                 }
-                            )
-                        }
-                    </Switch>
-
+                            }
+                        )
+                    }
+                </Switch>
 
 
             </div>
